c53a3c9a378270e72bc77f02ba3f2af14524fa6f
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-02-17 22:40:49 -0300

    Imported flowbite

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppComponent from 'app/app';
 import Home2 from 'app/home2';
+import 'flowbite';
 
 const rootEl = document.getElementById('root');
 const root = createRoot(rootEl);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
