4ada5d57b556ef60a9a6337703b35d16c6326c0d
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-02-17 22:40:49 -0300

    Imported flowbite

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -9,6 +9,7 @@ import { clearAuthentication } from 'app/shared/reducers/authentication';
 import ErrorBoundary from 'app/shared/error/error-boundary';
 import AppComponent from 'app/app';
 import { loadIcons } from 'app/config/icon-loader';
+import 'flowbite';
 
 const store = getStore();
 
