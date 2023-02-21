5a09b6b9537a4a51af111395f8304d27158fec35
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-02-21 18:54:21 -0300

    Removed some base code from JHipster

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-import getStore from 'app/config/store';
-import setupAxiosInterceptors from 'app/config/axios-interceptor';
-import { clearAuthentication } from 'app/shared/reducers/authentication';
 import ErrorBoundary from 'app/shared/error/error-boundary';
 import AppComponent from 'app/app';
-import { loadIcons } from 'app/config/icon-loader';
 import 'flowbite';
-
-const store = getStore();
-
-const actions = bindActionCreators({ clearAuthentication }, store.dispatch);
-setupAxiosInterceptors(() => actions.clearAuthentication('login.error.unauthorized'));
-
-loadIcons();
 
 const rootEl = document.getElementById('root');
 const root = createRoot(rootEl);
@@ -24,11 +10,9 @@ const root = createRoot(rootEl);
 const render = Component =>
   root.render(
     <ErrorBoundary>
-      <Provider store={store}>
-        <div>
-          <Component />
-        </div>
-      </Provider>
+      <div>
+        <Component />
+      </div>
     </ErrorBoundary>
   );
 
