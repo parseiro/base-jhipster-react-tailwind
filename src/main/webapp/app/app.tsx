ebbf91c07bb613e940df03bfa961cbf0e7660561
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-02-21 19:25:31 -0300

    Temporary changes

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -3,9 +3,12 @@ import './app.scss';
 import 'app/config/dayjs.ts';
 
 import React from 'react';
+import { Navbar } from 'app/modules/home/navbar';
 
 const baseHref = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
 
-export const App = () => {};
+export const App = () => {
+  return <Navbar />;
+};
 
 export default App;
