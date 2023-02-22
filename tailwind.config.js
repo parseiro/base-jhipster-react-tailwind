d0ca21256b9d8b459fc0b345df11d3105aa08a24
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-02-21 23:15:02 -0300

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#FFC9E5',
+          100: '#FFB4DB',
+          200: '#FF8BC7',
+          300: '#FF63B3',
+          400: '#FF3AA0',
+          500: '#FF118C',
+          600: '#D80070',
+          700: '#A00053',
+          800: '#680036',
+          900: '#300019',
+        },
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
